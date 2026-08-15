e2e5e943bea47befa2a6eaa371fb310bf61fd206
xcvzolda <166529366+xcvzolda@users.noreply.github.com>
2026-08-15 06:24:43 +0300

    Update bitcoin.cpp

--- src/qt/bitcoin.cpp
+++ src/qt/bitcoin.cpp
@@ -168,12 +168,12 @@ int main(int argc, char *argv[])
 
     // Application identification (must be set before OptionsModel is initialized,
     // as it is used to locate QSettings)
-    app.setOrganizationName("Tajcoin");
+    app.setOrganizationName("Microcoin");
     //XXX app.setOrganizationDomain("");
     if(GetBoolArg("-testnet", false)) // Separate UI settings for testnet
-        app.setApplicationName("Tajcoin-Qt-testnet");
+        app.setApplicationName("Microcoin-Qt-testnet");
     else
-        app.setApplicationName("Tajcoin-Qt");
+        app.setApplicationName("Microcoin-Qt");
 
     // ... then GUI settings:
     OptionsModel optionsModel;
